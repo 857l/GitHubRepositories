6e121cb9b027ace0d9fbdc4d10b16dce3622790e
857l <nik.gusev.2017@bk.ru>
2025-09-22 21:02:41 +0300

    add fragments

--- app/src/main/java/ru/n857l/githubrepositories/MainActivity.kt
+++ app/src/main/java/ru/n857l/githubrepositories/MainActivity.kt
@@ -1,14 +1,31 @@
 package ru.n857l.githubrepositories
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
+        androidx.core.view.WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.fragment_authentication)
+
+        val rootView = findViewById<View>(R.id.authenticationContainer)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { view, insets ->
+            val systemInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(
+                left = systemInsets.left,
+                top = systemInsets.top,
+                right = systemInsets.right,
+                bottom = systemInsets.bottom
+            )
+            WindowInsetsCompat.CONSUMED
+        }
     }
 }
